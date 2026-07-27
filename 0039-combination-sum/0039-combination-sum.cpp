e1b0ab2a744a5f63e3fa8b8f5559b1ef00b1b440
Adeebkhan21 <madeebkhan78@gmail.com>
2026-07-27 21:50:04 +0530

    Time: 3 ms (52.56%), Space: 14.4 MB (39.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        vector<int> path;
-        sort(candidates.begin(), candidates.end());
-        backtrack(candidates, 0, target, path, result);
-        return result;
+    vector<vector<int>> ans;
+    vector<int> temp;
+
+    void solve(int idx, vector<int>& candidates, int target) {
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (idx == candidates.size() || target < 0)
+            return;
+
+        if (candidates[idx] <= target) {
+            temp.push_back(candidates[idx]);
+            solve(idx, candidates, target - candidates[idx]);
+            temp.pop_back();
+        }
+
+        solve(idx + 1, candidates, target);
     }
 
-private:
-    void backtrack(const vector<int>& candidates, int start, int target, vector<int>& path, vector<vector<int>>& result) {
-        if (target == 0) {
-            result.push_back(path);
-            return;
-        }
-        if (target < 0) {
-            return;
-        }
-        for (int i = start; i < candidates.size(); ++i) {
-            path.push_back(candidates[i]);
-            backtrack(candidates, i, target - candidates[i], path, result);
-            path.pop_back();
-        }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        solve(0, candidates, target);
+        return ans;
     }
 };
