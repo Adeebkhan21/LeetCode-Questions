5eecd309d6fa268feb685d0535ee9ab5440509cc
Adeebkhan21 <madeebkhan78@gmail.com>
2026-09-08 23:17:30 +0530

    Time: 4 ms (34.8%), Space: 18.1 MB (5.69%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string str) {
+       
+        istringstream iss(str);
+        vector<std::string> words;
+        string word;
+        while (iss >> word) {
+            
+            reverse(word.begin(), word.end());
+            words.push_back(word);
+        }
+
+        string result = "";
+        for (const string reversedWord : words) {
+            result += reversedWord + " ";
+        }
+        result.pop_back();  
+        
+        return result;        
+    }
+};
